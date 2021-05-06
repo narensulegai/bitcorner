406c2bd1b22701a8114fa6978eba5dd9c5e9064b
sowmyadvn <sowmyadvn.92@gmail.com>
2021-05-06 12:39:25 -0700

    Currency auto-add when creating bank account

--- src/main/java/com/example/demo/api/BankAccountController.java
+++ src/main/java/com/example/demo/api/BankAccountController.java
@@ -1,13 +1,18 @@
 package com.example.demo.api;
 
+import com.example.demo.model.BalanceEntity;
 import com.example.demo.model.BankAccountEntity;
 import com.example.demo.model.CustomerEntity;
+import com.example.demo.Currency;
+import com.example.demo.repository.BalanceRepository;
 import com.example.demo.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @RestController
@@ -16,6 +21,9 @@ public class BankAccountController {
 
     @Autowired
     CustomerRepository customerRepository;
+    
+    @Autowired
+    BalanceRepository balanceRepository; 
 
     @ResponseBody
     @GetMapping
@@ -34,6 +42,18 @@ public class BankAccountController {
 
         customerEntity.setBankAccount(bankAccountEntity);
         customerRepository.save(customerEntity);
+        
+        
+        if(balanceRepository.findByBankAccountId(bankAccountEntity.getId()) == null) {
+        	for(Currency c: Currency.values()) {
+                BalanceEntity balanceEntity = new BalanceEntity();
+                balanceEntity.setBankAccount(bankAccountEntity);
+                balanceEntity.setBalance(BigDecimal.ZERO);
+                balanceEntity.setCurrency(c);
+                balanceRepository.save(balanceEntity);
+            }
+        }
+        
         return customerEntity.getBankAccount();
     }
 }
