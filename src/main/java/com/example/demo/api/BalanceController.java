3a0d417fd9c330d13fa041307643ca93354b4c8c
sowmyadvn <sowmyadvn.92@gmail.com>
2021-05-07 17:14:45 -0700

    Add exchange rates controller: /balance/exchangeRates

--- src/main/java/com/example/demo/api/BalanceController.java
+++ src/main/java/com/example/demo/api/BalanceController.java
@@ -3,12 +3,17 @@ package com.example.demo.api;
 import com.example.demo.model.BalanceEntity;
 import com.example.demo.model.CustomerEntity;
 import com.example.demo.repository.BalanceRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/balance")
@@ -40,5 +45,17 @@ public class BalanceController {
         }
         return balanceEntities.size();
     }
-
+    
+    @ResponseBody
+    @GetMapping(path = "/rates")
+    public Map<String, Object> getExchangeRates()  {
+    	ObjectMapper mapper = new ObjectMapper();
+    	Map<String, Object> rateMap = null;
+    	try {
+    	   rateMap = mapper.readValue(ClassLoader.getSystemClassLoader().getResourceAsStream("json/exchangeRates.json"), Map.class);
+    	} catch (IOException e) {
+    	    e.printStackTrace();
+    	}
+        return rateMap;
+    }
 }
