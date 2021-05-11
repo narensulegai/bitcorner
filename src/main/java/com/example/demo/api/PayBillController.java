53ac0a74a6a7ea97973936ba83af7a7cc4eab4b1
sowmyadvn <sowmyadvn.92@gmail.com>
2021-05-11 16:22:00 -0700

    bitcorner account

--- src/main/java/com/example/demo/api/PayBillController.java
+++ src/main/java/com/example/demo/api/PayBillController.java
@@ -1,17 +1,22 @@
 package com.example.demo.api;
 
 import com.example.demo.BillStatus;
+import com.example.demo.Currency;
 import com.example.demo.model.BalanceEntity;
+import com.example.demo.model.BankAccountEntity;
 import com.example.demo.model.BillEntity;
 import com.example.demo.model.CustomerEntity;
 import com.example.demo.repository.BalanceRepository;
+import com.example.demo.repository.BankAccountRepository;
 import com.example.demo.repository.BillRepository;
 import com.example.demo.repository.CustomerRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -34,6 +39,9 @@ public class PayBillController {
     
     @Autowired
     BalanceRepository balanceRepository;
+    
+    @Autowired
+    BankAccountRepository bankAccountRepository;
 
     @ResponseBody
     @GetMapping
@@ -55,11 +63,14 @@ public class PayBillController {
     }
     
     @ResponseBody
+    @Transactional
     @PutMapping
     public ResponseEntity<?> settleBill(@RequestBody @Valid BillEntity billEntity) {
     	// Payer adjust
         CustomerEntity customerEntity = (CustomerEntity) SecurityContextHolder.getContext()
                 .getAuthentication().getPrincipal();
+
+        double serviceFee = billEntity.getAmount() * 0.0001;
         Map<String, String> errors = new HashMap<>();
         BalanceEntity payerBalanceEntity = balanceRepository.findByBankAccountAndCurrency(customerEntity.getBankAccount(), billEntity.getCurrency());
         Integer payerBalance = payerBalanceEntity.getBalance();
@@ -67,7 +78,7 @@ public class PayBillController {
         	errors.put("err", "Not enough balance");
         }
         else {
-        	payerBalanceEntity.setBalance(payerBalance - billEntity.getAmount());
+        	payerBalanceEntity.setBalance(payerBalance - billEntity.getAmount() - (int) Math.ceil(serviceFee));
         	
         	
         	// failed to save payer balance
@@ -77,13 +88,45 @@ public class PayBillController {
     						.body(errors);
     		}
         }
-        // bitcorner adjust
-    	// CustomerEntity bitcorner = customerRepository.findByEmail("bitcorner@gmail.com");
+        // bitcorner adjust service fee
+        CustomerEntity bitcorner = customerRepository.findByEmail("bitcorner@gmail.com");
+        if(bitcorner == null) {
+        	bitcorner = new CustomerEntity();
+        	bitcorner.setEmail("bitcorner@gmail.com");
+        	bitcorner.setName("Bitcorner");
+            customerRepository.save(bitcorner);
+        } 
+        if (bitcorner.getBankAccount() == null) {
+        	BankAccountEntity bankAccountEntity = new BankAccountEntity();
+        	bankAccountEntity.setAddress("123 NYSE, New York");
+        	bankAccountEntity.setBankName("Chase");
+        	bankAccountEntity.setCountry("United States");
+        	bankAccountEntity.setOwnerName(bitcorner.getName());
+        	bankAccountEntity.setPrimaryCurrency(Currency.USD);
+        	bankAccountEntity.setAccountNumber("1111111111111");
+			bankAccountRepository.save(bankAccountEntity);
+        	bitcorner.setBankAccount(bankAccountEntity);
+            customerRepository.save(bitcorner);
+            
+            if(balanceRepository.findByBankAccountId(bankAccountEntity.getId()).size() == 0) {
+            	for(Currency c: Currency.values()) {
+                    BalanceEntity balanceEntity = new BalanceEntity();
+                    balanceEntity.setBankAccount(bitcorner.getBankAccount());
+                    balanceEntity.setBalance(0);
+                    balanceEntity.setCurrency(c);
+                    balanceRepository.save(balanceEntity);
+                }
+            }
+        }
+        BalanceEntity bitcornerBalanceEntity = balanceRepository.findByBankAccountAndCurrency(bitcorner.getBankAccount(), billEntity.getCurrency());
+        Integer bitcornerBalance = bitcornerBalanceEntity.getBalance();
+        bitcornerBalanceEntity.setBalance(bitcornerBalance + (int) Math.ceil(serviceFee));
+        balanceRepository.save(bitcornerBalanceEntity);
         
     	// payee adjust
     	BalanceEntity payeeBalanceEntity = balanceRepository.findByBankAccountAndCurrency(billEntity.getCustomer().getBankAccount(), billEntity.getCurrency());
-    	Integer balance = payeeBalanceEntity.getBalance();
-    	payeeBalanceEntity.setBalance(balance + billEntity.getAmount());
+    	Integer payeeBalance = payeeBalanceEntity.getBalance();
+    	payeeBalanceEntity.setBalance(payeeBalance + billEntity.getAmount());
 		
     	if(balanceRepository.save(payeeBalanceEntity) == null) {
 			errors.put("err", "Payee balance issue");
