98fe7ee752d4aa68eb96753a28c4dfcc4c0be6c6
sowmyadvn <sowmyadvn.92@gmail.com>
2021-05-11 01:20:06 -0700

    Payee balance add

--- src/main/java/com/example/demo/api/PayBillController.java
+++ src/main/java/com/example/demo/api/PayBillController.java
@@ -1,13 +1,19 @@
 package com.example.demo.api;
 
+import com.example.demo.model.BalanceEntity;
 import com.example.demo.model.BillEntity;
 import com.example.demo.model.CustomerEntity;
+import com.example.demo.repository.BalanceRepository;
 import com.example.demo.repository.BillRepository;
+import com.example.demo.repository.CustomerRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -16,6 +22,12 @@ public class PayBillController {
 
     @Autowired
     BillRepository billRepository;
+    
+    @Autowired
+    CustomerRepository customerRepository;
+    
+    @Autowired
+    BalanceRepository balanceRepository;
 
     @ResponseBody
     @GetMapping
@@ -34,5 +46,17 @@ public class PayBillController {
 
         billEntity.setEmail(customerEntity.getEmail());
         return billRepository.save(billEntity);
+    }
+    
+    @ResponseBody
+    @PutMapping
+    public BillEntity settleBill(@RequestBody @Valid BillEntity billEntity ) {
+    	
+    	// payee adjust
+    	BalanceEntity payeeBalanceEntity = balanceRepository.findByBankAccountAndCurrency(billEntity.getCustomer().getBankAccount(), billEntity.getCurrency());
+    	Integer balance = payeeBalanceEntity.getBalance();
+    	payeeBalanceEntity.setBalance(balance + billEntity.getAmount());
+		return billRepository.save(billEntity);
+    	
     }
 }
